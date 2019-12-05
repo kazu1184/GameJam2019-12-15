ca7b46c7658c85058c1d497c5e57d0806dd21a93
kazu <gambal.job@gmail.com>
2019-12-05 11:25:10 +0900

    ButtonTap

--- GameJam20191205/Assets/GameObject/UI/ButtonTap.cs
+++ GameJam20191205/Assets/GameObject/UI/ButtonTap.cs
@@ -6,15 +6,21 @@ public class ButtonTap : MonoBehaviour
 {
     [SerializeField]
     GameObject scoreObject;
+    [SerializeField]
+    GameObject timeObject;
     Score score;
+    TimeController time;
 
     private void Start()
     {
         score = scoreObject.GetComponent<Score>();
+        time = timeObject.GetComponent<TimeController>();
     }
 
     public void TapWarkOut()
     {
+        if (time.pTime <= 0)
+            return;
         score.pScore += 1;
     }
 }
--- GameJam20191205/Assets/GameObject/UI/Score.cs
+++ GameJam20191205/Assets/GameObject/UI/Score.cs
@@ -5,20 +5,20 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-    private float score;
+    private static float score;
     [SerializeField]
     private Text scoreText;
 
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(this.gameObject);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = score.ToString();
+        scoreText.text = "Score::"+score.ToString();
     }
 
     public float pScore
