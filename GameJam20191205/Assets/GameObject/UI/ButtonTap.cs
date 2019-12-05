bd4a0774084e3f8e45f9a02ca6a461d9fe0b9fcd
kazu <gambal.job@gmail.com>
2019-12-05 10:43:15 +0900

    <Score><Time>

---
+++ GameJam20191205/Assets/GameObject/UI/ButtonTap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTap : MonoBehaviour
+{
+    [SerializeField]
+    GameObject scoreObject;
+    Score score;
+
+    private void Start()
+    {
+        score = scoreObject.GetComponent<Score>();
+    }
+
+    public void TapWarkOut()
+    {
+        score.pScore += 1;
+    }
+}
---
+++ GameJam20191205/Assets/GameObject/UI/Score.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private float score;
+    [SerializeField]
+    private Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    public float pScore
+    {
+        get { return score; }
+        set { score = value; }
+    }
+}
